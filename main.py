6c240db1259f456c2e96fa5a7aba11fab68b5820
nicoladen05 <nicolashartmanntaba@gmail.com>
2022-05-28 11:12:16 +0200

    add history command

--- main.py
+++ main.py
@@ -137,7 +137,11 @@ async def rank(ctx, *, nametag):
         )
 
         #rank icon
-        rank_int = int(stats['currenttier'])
+        if stats['currenttier'] == None:
+            rank_int = 0 
+        else:
+            rank_int = int(stats['currenttier'])
+
         rank_icon_url = f'https://media.valorant-api.com/competitivetiers/564d8e28-c226-3180-6285-e48a390db8b1/{rank_int}/smallicon.png'
 
         # set embed thumbnail
@@ -147,12 +151,19 @@ async def rank(ctx, *, nametag):
 
         mmr = stats['mmr_change_to_last_game']
 
-        embed.add_field(name='Current Rank', value=stats['currenttierpatched'], inline=True)
+        if stats['currenttierpatched'] == None:
+            rank = 'Unranked'
+        else:
+            rank = stats['currenttierpatched']
 
-        if mmr > 0:
-            embed.add_field(name='Last Match', value='+' + str(mmr), inline=True)
-        elif mmr < 0:
-            embed.add_field(name='Last Match', value=str(mmr), inline=True)
+        embed.add_field(name='Current Rank', value=rank, inline=True)
+
+
+        if mmr != None:
+            if mmr > 0:
+                embed.add_field(name='Last Match', value='+' + str(mmr), inline=True)
+            elif mmr < 0:
+                embed.add_field(name='Last Match', value=str(mmr), inline=True)
 
         embed.add_field(name='Elo', value=stats['ranking_in_tier'], inline=True)
 
@@ -183,6 +194,118 @@ async def rank(ctx, *, nametag):
         await ctx.send(embed=embed)
 
 
+# history command
+@client.command()
+async def history(ctx, *, nametag):
+    # split nametag into name and tag at #
+    name_split = nametag.split('#')
+
+    name = name_split[0]
+    tag = name_split[1]
+
+    # make a get request
+    url = 'https://api.henrikdev.xyz/valorant/v3/matches/eu/' + name + '/' + tag
+    response = get(url, headers=headers)
+
+    # check if the response is valid
+    if response.status_code == 200:
+            
+            json_response = json.loads(response.text)
+
+            stats = json_response['data']
+
+            # send title embed
+            embed = discord.Embed(
+                title=name + "'s History",
+                color=0x00ff00
+            )
+
+            # send embed
+            await ctx.send(embed=embed)
     
+            #make a for loop to get the metadata the last 5 games
+            for i in range(0, 5):
+                embed = discord.Embed(
+                    color=0x00ff00
+                )
+
+                # get data from metadata
+                map = stats[i]['metadata']['map']
+                start_unix = stats[i]['metadata']['game_start']
+                mode = stats[i]['metadata']['mode']
+
+                # search for name in stats[i]['players']
+                for j in range(0, len(stats[i]['players']['all_players'])):
+                    if stats[i]['players']['all_players'][j]['name'] == name:
+                        player_stats = stats[i]['players']['all_players'][j]
+                        break
+                
+                player_team = player_stats['team']
+                player_character = player_stats['character']
+                player_character_icon = player_stats['assets']['agent']['small']
+                kills = player_stats['stats']['kills']
+                deaths = player_stats['stats']['deaths']
+                assists = player_stats['stats']['assists']
+
+
+                # total shots hit
+                total_shots_hit = player_stats['stats']['headshots'] + player_stats['stats']['bodyshots'] + player_stats['stats']['legshots']
+                # check if total_shots_hit is 0 so it doesnt break the bot
+                if total_shots_hit == 0:
+                    total_shots_hit = 1
+                # raw hs percent
+                headshot_percent_raw = player_stats['stats']['headshots'] / total_shots_hit
+                # calculate headshot_percent_raw into a percent value
+                headshot_percent = str(round(headshot_percent_raw * 100, 2)) + '%'
+
+                # check if the player has won
+                if stats[i]['teams'][str(player_team).lower()]['has_won']:
+                    won = True
+                else:
+                    won = False
+
+                # get exact score
+                rounds_won = stats[i]['teams'][str(player_team).lower()]['rounds_won']
+                rounds_lost = stats[i]['teams'][str(player_team).lower()]['rounds_lost']
+
+                # make embed
+                embed.add_field(name='Map', value=map, inline=True)
+                embed.add_field(name='Mode', value=mode, inline=True)
+                embed.add_field(name='Time', value=f'<t:{start_unix}:R>', inline=True)
+                embed.add_field(name='Team', value=player_team, inline=True)
+                embed.add_field(name='Character', value=player_character, inline=True)
+                embed.add_field(name='Kills', value=kills, inline=True)
+                embed.add_field(name='Deaths', value=deaths, inline=True)
+                embed.add_field(name='Assists', value=assists, inline=True)
+                embed.add_field(name='Headshots', value=headshot_percent, inline=True)
+                embed.add_field(name='Won', value=won, inline=True)
+                embed.add_field(name='Rounds Won', value=rounds_won, inline=True)
+                embed.add_field(name='Rounds Lost', value=rounds_lost, inline=True)
+                embed.set_thumbnail(url=player_character_icon)
+
+                await ctx.send(embed=embed)
+
+    
+
+    elif response.status_code == 404:
+        # make a embed
+        embed = discord.Embed(
+            title="Error",
+            description="The player you searched for doesn't exist",
+            color=0xff0000
+        )
+
+        await ctx.send(embed=embed)
+
+    else:
+        # send error embed
+        embed = discord.Embed(
+            title="Error",
+            description="An error occured",
+            color=0xff0000
+        )
+
+        await ctx.send(embed=embed)
+
 
 client.run('OTc5ODI3MjExNjUxNjQ1NTAw.GbxAJm.3B5sv0vgimID0nvTKi9U_y7aL2xDoelOncOkB8')
